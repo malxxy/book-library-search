0b23e68b1200dfc47fe7ef97f05af3723bd5347f
malxxy <maliayraguen@gmail.com>
2023-04-18 23:59:17 -0700

    login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,10 +3,11 @@ import { gql } from '@apollo/client';
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!,) {
     login(email: $email, password: $password) {
-      email
-      password
-    }
-    token
+      user {
+        email
+        password
+        }
+      token
   }
 `;
 

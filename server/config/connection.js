59d23d59f1640e258e216b69b45459c9519a35fb
malxxy <maliayraguen@gmail.com>
2023-04-18 20:39:32 -0700

    restart application  before npm i

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 require ('dotenv').config();
-console.log("process",process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useCreateIndex: true,
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;

fe8ca91965b2508c7a4a82298bc71908af4e5676
malxxy <maliayraguen@gmail.com>
2023-04-18 18:59:28 -0700

    package rehaul

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require ('dotenv').config();
 console.log("process",process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:3000/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
--- server/server.js
+++ server/server.js
@@ -25,9 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
-} else if (process.env.NODE_ENV !== 'production'){
-  require('dotenv').config({path:'../.env'})
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);

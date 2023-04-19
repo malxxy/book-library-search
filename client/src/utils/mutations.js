c495cdf456ee102df1d8cb677a5b4174d7106c95
malxxy <maliayraguen@gmail.com>
2023-04-19 10:56:20 -0700

    return to user id

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -28,7 +28,7 @@ export const CREATE_USER = gql`
 export const SAVE_BOOK = gql`
     mutation saveBook($bookId: bookId!, $authors:[String], $description: String, $image: String, $link: String, $title: String!) {
         saveBook(bookId: $bookId, authors: $authors, description: $description, image: $image, link: $link, title: $title) {
-            bookId
+            _id
             username
             email
             savedBooks {

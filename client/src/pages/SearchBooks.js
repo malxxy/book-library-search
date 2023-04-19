78d794a1a99ff56de2334eb87590ea7f74521a61
malxxy <maliayraguen@gmail.com>
2023-04-19 12:06:26 -0700

    trial of passing jsut id through variable

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -39,7 +39,7 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
-
+      console.log("Book Data",bookData);
       setSearchedBooks(bookData);
       setSearchInput('');
     } catch (err) {
@@ -51,7 +51,8 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    console.log("book to save variable",bookToSave);
+    const passedId = bookToSave.bookId;
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -62,13 +63,13 @@ const SearchBooks = () => {
     // add  try catch
     try {
       const data = await saveBook({
-        variables: bookToSave
+        variables: passedId
       })
-      console.log("what is this", data);
-      console.log("what about this",data.data);
+      console.log("what is data", data);
+      console.log("what about data",data.data);
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      setSavedBookIds([...savedBookIds, data.data]);
       console.log(savedBookIds)
 
     } catch (err) {
